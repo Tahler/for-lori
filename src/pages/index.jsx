9b471cfd2b212dc70a5c751f79a618183e6128e5
Tyler Berry <tyler@berryac.com>
2018-03-28 20:02:41 -0500

    wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,36 @@ const Intro = () => (
     <div className={styles.intro}>
         <h1>Captivating Corsages</h1>
         <Button>Order</Button>
+        <Nav />
     </div>
 );
+
+const Nav = () => {
+    // TODO scroll spy and remove absolute positioning
+    return (
+        <nav style={{
+            bottom: 0,
+            position: 'absolute',
+            // float: 'right',
+            // flexAlign: 'end',
+        }}>
+            <ul style={{
+                display: 'block',
+                float: 'left',
+                listStyle: 'none',
+            }}>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    );
+};
 
 const Tile = ({ src }) => (
     <div style={{
         padding: '10px',
     }}>
-        <img style={{margin: '0'}} src={src} alt="tile" />
+        <img style={{margin: 0}} src={src} alt="tile" />
     </div>
 );
 

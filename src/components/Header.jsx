0a8322dc2979b0e7ddc88a10920fd19a9fd11560
Tyler Berry <tyler@berryac.com>
2019-10-28 22:58:47 -0700

    WIP

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import logo from '../assets/images/logo.svg';
+
+const Header = () => (
+  <header id="header" className="alt">
+    <span className="logo">
+      <img src={logo} alt="" />
+    </span>
+    <h1>Stellar</h1>
+    <p>
+      Just another free, fully responsive site template <br />
+      design by <a href="https://html5up.net">HTML5 UP</a>.
+    </p>
+  </header>
+);
+
+export default Header;
